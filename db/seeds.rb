7e40fe3cee6fa5fd09d51fc5757831275297977b
jordan <jordan.g.burk@gmail.com>
2016-06-28 15:55:45 -0700

    fixed seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create({username: jyllian, password: demoaccount});
-User.create({username: wicker, password: wicker});
+User.create({username: "jyllian", password: "demoaccount"});
+User.create({username: "wicker", password: "wicker"});
